e9b024267046f446878a6da335863451a8ab060a
Akila Mendis <akila@oneit.com.au>
2021-01-19 08:55:31 +0530

    returned token when user created

--- user/views.py
+++ user/views.py
@@ -1,12 +1,37 @@
+from django.http import HttpResponse
 from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from user.serializers import UserSerializer, AuthTokenSerializer
-
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from django.contrib.auth import get_user_model, authenticate
+from rest_framework import serializers
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system"""
     serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        self.create(request, *args, **kwargs)
+
+        email = request.data['email']
+        password = request.data['password']
+
+        user = authenticate(
+            request=request,
+            username=email,
+            password=password
+        )
+
+        if not user:
+            msg = _('Unable to authenticate with provided credentials')
+            raise serializers.ValidationError(msg, code='authentication')
+
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key
+        })
 
 
 class CreateTokenView(ObtainAuthToken):
